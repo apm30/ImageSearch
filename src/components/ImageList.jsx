a2d8830be4ab8d8590290e890d323855b17bd1f2
Domo <stolarczyk.dominik@gmail.com>
2020-01-17 10:33:59 +0100

    key added

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function ImageList(props) {
 	const images = props.images.map(image => {
-		return <img src={image.urls.regular} alt="" />;
+		return (
+			<img key={image.id} alt={image.description} src={image.urls.regular} />
+		);
 	});
 	return <div>{images}</div>;
 }
